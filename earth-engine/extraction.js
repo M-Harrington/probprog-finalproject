60a5e15e1f6a2c9575d668cde091e0e0dabfeafe
Matt Harrington <42118783+M-Harrington@users.noreply.github.com>
2019-11-12 21:46:28 -0500

    added visualize for export

--- earth-engine/extraction.js
+++ earth-engine/extraction.js
@@ -22,20 +22,12 @@ for (i = 0; i < 4*17; i++) {
 
 
 
-// Years + months compiled to extract data over TODO: make proper dates
-var collection = ee.ImageCollection("LANDSAT/LE07/C01/T1")
-  .filterDate('2003-01-01', '2003-05-01')
-  .filterBounds(geometry);
- 
- Map.addLayer(goodComposite,
-             {bands: ['B3', 'B2', 'B1'], gain: 3.5},
-             'good composite');
              
 
 
 // Export the image, specifying scale and region.
 Export.image.toDrive({
-  image: classified,
+  image: classified.visualize({min: 0, max: 1, palette: palette}),
   description: 'imageToDriveTest',
   scale: 30,
   'crs':'EPSG:4326',
